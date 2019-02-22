1a26b3b434b2175a753837fcb80eac9b4c946b4b
Ravipati_V <veeravenkatasiva.ravipati@floridadep.gov>
2019-02-22 15:34:00 -0500

    Project copied from Github

---
+++ src/main/java/java8/FileIsPresent.java
@@ -0,0 +1,17 @@
+package generics;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class FileIsPresent {
+    public static void main(String... args) {
+        String directory = "\\\\OCULUSAPPS\\xgate_testbed\\";
+        System.out.println(directory);
+        String fileName = directory.concat("128780_hw.doc");
+        File f = new File(fileName);
+        if(f.exists() && !f.isDirectory()) {
+            System.out.println("file exist");
+            System.out.println(f.getName());
+}
+    }
+}
