b05a656f8523ceb7a6072691b22c879fa430ec86
Ravipati_V <veeravenkatasiva.ravipati@floridadep.gov>
2019-03-14 21:26:44 -0400

    added ImperitveVsDeclarative

--- src/main/java/java8/DirectoryStreamEx.java
+++ src/main/java/java8/DirectoryStreamEx.java
@@ -3,24 +3,32 @@ package java8;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class FilterFile {
     public static void main(String... args) throws IOException {
         Arrays.stream(new File(".").listFiles()).forEach(file -> file.getName());
-        System.out.println(Paths.get(".").getClass());
-        System.out.println(Paths.get("."));
-        Files.list((Paths.get("."))).forEach(System.out::println);
+
+        //Without using Directory stream
         String[] fileNames = new File(".").list(new FilenameFilter() {
             @Override
             public boolean accept(File file, String name) {
-                return name.endsWith(".class");
+                return name.endsWith(".java");
             }
         });
         for (String fileName : fileNames) {
             System.out.println(fileName);
+        }
+
+        Files.newDirectoryStream(Paths.get("."), new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path entry) throws IOException {
+                return false;
+            }
         }
     }
 }
