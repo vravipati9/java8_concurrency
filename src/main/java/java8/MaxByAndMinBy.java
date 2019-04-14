95a6ca322e74df421bf4e689bc05ee819d699fe2
Veera <vravipati9@gmail.com>
2019-04-14 08:17:03 -0400

    work with FlatMap, groupingBy etcc

--- src/main/java/java8/MaxByAndMinBy.java
+++ src/main/java/java8/MaxByAndMinBy.java
@@ -1,32 +1,35 @@
 package generics;
 import java8.Person;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static java8.Person.getCreatePeople;
+import static java8.Person.getAllPersons;
 
 /*
-Group By first character and find older person in that group
+    Group with first Character, Person with same length for name, then select the elder from the two
+    Else, person with longer names comes out
  */
-public class MaxByAndMinBy {
+public class ReducingMethod {
     public static void main(String... args) {
-        Function<Person, Character> firstLetter = o -> o.getFirstName().charAt(0);
 
-        Comparator<Person> byAge = new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                return o1.getAge() > o2.getAge() ? 1 : -1;
+        Function<Person, Character> firstCharacter = person -> person.getFirstName().charAt(0);
+        BinaryOperator<Person> classifier = (person, person2) -> {
+            if (person.getFirstName().length() == person2.getFirstName().length()) {
+                return person.getAge() > person2.getAge() ? person : person2;
+            } else {
+                return person.getFirstName().length() > person.getFirstName().length() ? person : person2;
             }
         };
-        Map<Character, Optional<Person>> groupedByFirstCharacter = getCreatePeople().stream().
-                collect(Collectors.groupingBy(firstLetter, Collectors.maxBy(byAge)));
 
-        System.out.println(groupedByFirstCharacter);
+        getAllPersons().stream().
+                collect(Collectors.groupingBy(firstCharacter)).forEach((character, personList) -> System.out.println(personList));
+
+        System.out.println("* * * Formatting results based on conditions * * *");
+        getAllPersons().stream().
+                collect(Collectors.groupingBy(firstCharacter, Collectors.reducing(classifier)))
+                        .forEach((integer, personList) -> System.out.println(personList));
+
     }
 }
