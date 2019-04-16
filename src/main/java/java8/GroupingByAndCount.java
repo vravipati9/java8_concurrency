e685908adc31d0d3bb824492a02daf6bba2c1ad9
Veera <vravipati9@gmail.com>
2019-04-15 22:18:11 -0400

    groupingBy and counting

--- src/main/java/java8/GroupingByAndCount.java
+++ src/main/java/java8/GroupingByAndCount.java
@@ -3,6 +3,8 @@ package java8;
 import pojos.Person;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static pojos.Person.*;
 
@@ -10,5 +12,7 @@ public class GroupingByAndCount {
     public static void main(String[] args) {
         List<Person> persons = getAllPersons();
 
+        Map<String, Long> personMap =  persons.stream().collect(Collectors.groupingBy(person -> person.getFirstName(), Collectors.counting()));
+        System.out.println(personMap);
     }
 }
